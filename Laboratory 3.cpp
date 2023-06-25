96ad5acd29eceeff3f8205f1aa575037dbbaff96
Bohdan Patrin <127937644+BohdanPatrin@users.noreply.github.com>
2023-06-25 21:52:25 +0300

    Add files via upload

---
+++ Laboratory 3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	long long* pi = new long long;
+	long long* otherpi = new long long;
+	int d = otherpi - pi;
+	int d_temp = d;
+	unsigned short int counter = 0;
+	cout << "<table border=\"2\">\n";
+	cout << "<th>i</th> <th>pi</th><th>d</th>";
+	for (int i = 1; i < 20; i++) {
+		cout << "<tr>\n";
+		pi = otherpi;
+		otherpi = new long long;
+		d = otherpi - pi;
+		if (d == d_temp) {
+			counter++;
+			if (counter == 10) {
+				cout << "\nthe number of coincidences of d values has reached the limit\n";
+					break;
+			}
+		}
+		d_temp = d;
+		cout << "<td>" << i << "</td>\n";
+		cout << "<td>" << pi << "</td>\n";
+		cout << "<td>" << d << "</td>\n";
+		cout << "</tr>\n";
+	}
+	delete pi;
+	delete otherpi;
+}
